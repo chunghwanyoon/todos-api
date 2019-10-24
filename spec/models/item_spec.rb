84fbf3a7f577284b2b203cb7c7467e77f276edb8
Chunghwan Yoon <chunghwan@redrocket.co.kr>
2019-10-24 16:12:59 +0900

    write model specs

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+
+# Test suite for the item model
+RSpec.describe Item, type :model do
+  # Association test
+  # ensure an item record belongs to a single todo record
+  it { should belongs_to(:todo) }
+
+  # Validation test
+  # ensure column name is present before saving
+  it { should validate_presence_of(:name) }
+  
+end
+
+# RSpec.describe Item, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
---
+++ spec/models/todo_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+# Test suite for the Todo model
+RSpec.describe Todo, type: :model do
+  
+  # Association test
+  # ensure Todo model has a 1:m relationship with the item model
+  it { should have_many(:items).dependent(:destory) }
+
+  # Validation tests
+  # ensure columns title and created_by are present before saving
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:created_by) }
+
+end
+
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end

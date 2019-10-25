eae0a8d33dd880311757c08e4bdb2efc304942ae
Chunghwan Yoon <chunghwan@redrocket.co.kr>
2019-10-25 17:26:50 +0900

    include module to application controller to notify

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+    # controller classes don't know about these helpers yet
+    # so including these modules to application controller
+    include Response
+    include ExceptionHandler
 end

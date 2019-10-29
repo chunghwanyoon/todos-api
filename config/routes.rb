03076f1b8c584db1d82fa54b4fd200a85e0d99a9
Chunghwan Yoon <chunghwan@redrocket.co.kr>
2019-10-29 11:55:50 +0900

    api버전관리 관련 내용 추가

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  resources :todos do
-    resources :items
-  end
 
+  # module the controller without affecting the URI
+  scope module: :v2, constraints: ApliVersion.new('v2') do
+    resources :todos, only: :index
+  end
+  
+  # namespace the controllers without affecting the URI
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :todos do
+      resources :items
+    end
+  end
+  
   # add routing for authentication action
   post 'auth/login', to: 'authentication#authenticate'
   
